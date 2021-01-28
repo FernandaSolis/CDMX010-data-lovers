30a216f2198de83434d8a293b93be887d785fc0f
FernandaSolis <fersolis518@gmail.com>
2021-01-27 20:14:07 -0600

    changes hu1

--- src/data.js
+++ src/data.js
@@ -7,4 +7,12 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+//van a tomar los valores que se les asgine (parametros)
+export const filterCharacters = (characters, filter, value) => {
+  //Filter recibe una funcion highorderfunction
+  const filteredCharacters = characters.filter((character) => {
+    return character[filter] === value // true | false
+  })
 
+  return filteredCharacters
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,46 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import dataRM from './rickandmorty.js';
 
-import data from './data/rickandmorty/rickandmorty.js';
+import dataRM from './data/rickandmorty.js';
+//import { filterByGender, filterByStatus } from './data.js'
+
+document.addEventListener('DOMContentLoaded',function(){
+    buildCard(dataRM.results)
+})
+let characterCard=document.querySelector('.character')
+
+/* const selector = document.getElementById('selector')
+
+selector.addEventListener('change', (event) => {
+    const gender = event.target.value
+    const filteredCharacters = filterByGender(dataRM.results, gender)
+
+    buildCard(filteredCharacters)
+})
+
+selector2.addEventListener('change', (event) => {
+    const status = event.target.value
+    const filteredCharacters = filterByStatus(dataRM.results, status)
+
+    buildCard(filteredCharacters)
+})
+ */
+function buildCard(data){
+    
+    data.forEach(characters => {
+        let imageCharacter=document.createElement('img')
+        imageCharacter.src=`${characters.image}`
+        characterCard.appendChild(imageCharacter)
+
+        let titleName=document.createElement('h1')//<p></p>
+        titleName.textContent=`${characters.name}` //aqui usar inner html con los  <p>id nombre</p>
+        characterCard.appendChild(titleName) //<div class="character"> <p>nombre</p> </div>
+
+        let parrafoId=document.createElement('p')
+        parrafoId.textContent=`${characters.id}`
+        characterCard.appendChild(parrafoId)
 
 
-console.log(example, data);
+    });
+
+}
+

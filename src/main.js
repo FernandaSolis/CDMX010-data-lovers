f44757f3e9fbbacf8f6d445a0878507146001b38
FernandaSolis <fersolis518@gmail.com>
2021-02-09 00:29:03 -0600

    test

--- src/main.js
+++ src/main.js
@@ -1,150 +1,121 @@
 import dataRM from './data/rickandmorty.js';
 import { search, orderData, filterByGender, filterByStatus, filterBySpecies } from './data.js'
 
-document.addEventListener('DOMContentLoaded',function(){
-    buildCard(dataRM.results)    
-})
-let characterCard=document.querySelector('.character')
- //Imprime Card de personajes
- function buildCard(data){
-    
+document.addEventListener('DOMContentLoaded', function () {
+    buildCard(dataRM.results)
+});
+let characterCard = document.querySelector('.character')
+//Card characters
+function buildCard(data) {
     data.forEach(character => {
-        let {image,name,id}=character;
-
-        let setCard=document.createElement('div')
-            setCard.innerHTML=`
+        let { image, name, id } = character;
+        let setCard = document.createElement('div')
+        setCard.innerHTML = `
             <div class="characterCard">
                 <div class="imageCharacter">
                     <img src = "${image}"/> </div>
                 <div class="info">
                     <h1 class="name">${name}</h1>
                     <p class="number"> # ${id} </p>
-                    <button id=${id}>ver detalles</button>
                 </div>
             </div> 
-        ` 
-        characterCard.appendChild(setCard) 
+        `
+        characterCard.appendChild(setCard)
     });
 }
-//Ordenamiento
- const order = document.getElementById('select-order');
-    order.addEventListener ('change', (event) => {
+//Order
+const order = document.getElementById('select-order');
+order.addEventListener('change', (event) => {
     const sortOrder = event.target.value;
     const orderedData = orderData(dataRM.results, 'name', sortOrder)
-    characterCard.innerHTML=''
+    characterCard.innerHTML = ''
     buildCard(orderedData)
-})  
+})
 //Search
 const searchDos = document.getElementById('search');
-    searchDos.addEventListener ('change', (event) => {
-        //event.preventDefault();           
-        const bar = event.target.value;
-        const barSearch = search(dataRM.results, bar);
-       
-            characterCard.innerHTML=''
-            buildCard(barSearch)        
-    })
-    
+searchDos.addEventListener('change', (event) => {
+    const bar = event.target.value;
+    const barSearch = search(dataRM.results, bar);
+    characterCard.innerHTML = ''
+    buildCard(barSearch)
+});
 //Filter Status
 const radioAlive = document.getElementById('alive');
 radioAlive.addEventListener('click', (event) => {
-    const status = event.target.value;     
-    const filteredCharacters = filterByStatus(dataRM.results, status) 
-    characterCard.innerHTML=''
+    const status = event.target.value;
+    const filteredCharacters = filterByStatus(dataRM.results, status)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioDead = document.getElementById('dead');
 radioDead.addEventListener('click', (event) => {
-    const status = event.target.value;     
-    const filteredCharacters = filterByStatus(dataRM.results, status) 
-    characterCard.innerHTML=''
+    const status = event.target.value;
+    const filteredCharacters = filterByStatus(dataRM.results, status)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioUnknownUno = document.getElementById('unknown');
 radioUnknownUno.addEventListener('click', (event) => {
-    const status = event.target.value;     
-    const filteredCharacters = filterByStatus(dataRM.results, status) 
-    characterCard.innerHTML=''
+    const status = event.target.value;
+    const filteredCharacters = filterByStatus(dataRM.results, status)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
-//Filter Género
+});
+//Filter Gender
 const radioFemale = document.getElementById('female');
 radioFemale.addEventListener('click', (event) => {
-    const gender = event.target.value;     
-    const filteredCharacters = filterByGender(dataRM.results, gender) 
-    characterCard.innerHTML=''
+    const gender = event.target.value;
+    const filteredCharacters = filterByGender(dataRM.results, gender)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioMale = document.getElementById('male');
 radioMale.addEventListener('click', (event) => {
-    const gender = event.target.value;     
-    const filteredCharacters = filterByGender(dataRM.results, gender) 
-    characterCard.innerHTML=''
+    const gender = event.target.value;
+    const filteredCharacters = filterByGender(dataRM.results, gender)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioUnknownGender = document.getElementById('unknownGender');
 radioUnknownGender.addEventListener('click', (event) => {
-    const gender = event.target.value;     
-    const filteredCharacters = filterByGender(dataRM.results, gender) 
-    characterCard.innerHTML=''
+    const gender = event.target.value;
+    const filteredCharacters = filterByGender(dataRM.results, gender)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 //Filter Species
 const radioHuman = document.getElementById('human');
 radioHuman.addEventListener('click', (event) => {
-    const species = event.target.value;     
-    const filteredCharacters = filterBySpecies(dataRM.results, species) 
-    characterCard.innerHTML=''
+    const species = event.target.value;
+    const filteredCharacters = filterBySpecies(dataRM.results, species)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioAlien = document.getElementById('alien');
 radioAlien.addEventListener('click', (event) => {
-    const species = event.target.value;     
-    const filteredCharacters = filterBySpecies(dataRM.results, species) 
-    characterCard.innerHTML=''
+    const species = event.target.value;
+    const filteredCharacters = filterBySpecies(dataRM.results, species)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioHumanoid = document.getElementById('humanoid');
 radioHumanoid.addEventListener('click', (event) => {
-    const species = event.target.value;     
-    const filteredCharacters = filterBySpecies(dataRM.results, species) 
-    characterCard.innerHTML=''
+    const species = event.target.value;
+    const filteredCharacters = filterBySpecies(dataRM.results, species)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioAnimal = document.getElementById('animal');
 radioAnimal.addEventListener('click', (event) => {
-    const species = event.target.value;     
-    const filteredCharacters = filterBySpecies(dataRM.results, species) 
-    characterCard.innerHTML=''
+    const species = event.target.value;
+    const filteredCharacters = filterBySpecies(dataRM.results, species)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
+});
 const radioRobot = document.getElementById('robot');
 radioRobot.addEventListener('click', (event) => {
-    const species = event.target.value;     
-    const filteredCharacters = filterBySpecies(dataRM.results, species) 
-    characterCard.innerHTML=''
+    const species = event.target.value;
+    const filteredCharacters = filterBySpecies(dataRM.results, species)
+    characterCard.innerHTML = ''
     buildCard(filteredCharacters)
-})
-/* const selector = document.getElementById('selector')
-
-selector.addEventListener('change', (event) => {
-    const gender = event.target.value
-    const filteredCharacters = filterByGender(dataRM.results, gender)
-
-    buildCard(filteredCharacters)
-})
-
-/*
-selector2.addEventListener('change', (event) => {
-    const status = event.target.value
-    const filteredCharacters = filterByStatus(dataRM.results, status)
-
-    buildCard(filteredCharacters)
-})
- */
-
-//  console.log(filterByGender(dataRM.results, 'Female'))
-// console.log(filterByStatus(dataRM.results, 'Alive'))
-//console.log(orderData(dataRM.results, 'name', 'nameAz' ));
-//console.log(orderData(dataRM.results, 'name', 'nameZa' ));
-//console.log(search (dataRM.results, 'Morty Smith'));
+});
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,35 +1,56 @@
-import {buidlCard} from '../src/main.js';
-import {example, anotherExample} from '../src/data.js'
+import { filterByGender, filterBySpecies, filterByStatus, orderData, search } from '../src/data.js'
+import { setCard } from '../src/main.js'
+const personajes = [
+  {
+    "id": 1,
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+  },
+  {
+    "id": 6,
+    "name": "Abadango Cluster Princess",
+    "status": "Alive",
+    "species": "Alien",
+    "gender": "Female",
+  },
+  {
+    "id": 46,
+    "name": "Bill",
+    "status": "unknown",
+    "species": "Animal",
+    "gender": "Male",
+  },
+];
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe('Funcion que devuelve estados de vida', () => {
+  it('El filtro devuelve unknown', () => {
+    expect(filterByStatus(personajes, "unknown")).toEqual([personajes[2]]);
   });
 });
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe('Funcion que devuelve géneros', () => {
+  it('El filtro devuelve femeninos', () => {
+    expect(filterByGender(personajes, "Female")).toEqual([personajes[1]]);
   });
 });
-
-/* 
-describe('test para revisar que builCard efectivamente sea una funcion', () => {
-  it('es una funcion', () => {
-    expect(typeof buidlCard).toBe('undefined');
+describe('Funcion que devuelve especies', () => {
+  it('El filtro devuelve humanos', () => {
+    expect(filterBySpecies(personajes, "Human")).toEqual([personajes[0]]);
   });
-
-  it('returns `buildCard`', () => {
-    expect(buidlCard()).toBe('character');
+});
+describe('Funcion que devuelve nombres', () => {
+  it('El filtro devuelve busqueda de nombres', () => {
+    expect(search(personajes, "Rick Sanchez")).toEqual([personajes[0]]);
   });
-});  */
+});
+/* describe('Funcion que devuelve data ordenada', () => {
+  it('El filtro devuelve orden alfabético', () => {
+    expect(orderData(personajes.name)).toBe([personajes[0, 1, 2]]);
+  });
+});
+describe('Funcion que devuelve data impresa', () => {
+  it('Impresion de Cards', () => {
+    expect(setCard(personajes.name)).toBe([personajes[0, 1, 2]]);
+  });
+}); */
